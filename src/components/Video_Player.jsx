26eacbede863c91e6159833a02742414dddcd10c
Marcin-Wiglusz <marcin.m.wiglusz@gmail.com>
2017-10-20 15:38:39 +0200

    working iframe

--- src/components/Video_Player.jsx
+++ src/components/Video_Player.jsx
@@ -4,13 +4,18 @@ export default class VideoPlayer extends React.Component{
 
   render() {
     let video = this.props.video;
-    console.log(video);
+    // App is rendering before YT fills videos array and video is undefined.
+    //if there is no video yet, return below div
     if (!video) {
       return <div>Wait for it...</div>
     }
+
+    let id = video.id.videoId;
+    let url = `https://www.youtube.com/embed/${id}`;
+
     return(
       <div>
-        <iframe src = {video.snippet.thumbnails.default.url}></iframe>
+        <iframe src = {url}></iframe>
         <div>
           <div>{video.snippet.title}</div>
           <div>{video.snippet.description}</div>
